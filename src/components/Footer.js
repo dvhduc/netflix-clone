db41e4569e9961e28a8bd9ef6140bfb71a11f0af
dvhduc <01627dlm@gmail.com>
2021-05-28 21:40:00 +0700

    Done

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const footer = () => (
+  <footer className="footer">
+    <div className="footer__copyright">
+      &copy; 2021 Made by{' '}
+      <a className="footer__copyright--link" href="">
+        {' '}
+        Shin Ji
+      </a>
+    </div>
+  </footer>
+);
+
+export default footer;
